5846378b5c0d311957e1c4b632ae4ce7b278e9f2
FranciscMoldovan <franciscmoldovan93@gmail.com>
2015-05-12 17:56:45 +0300

    Ability to draw brush MVC Style

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,8 +1,13 @@
 package controller;
 
+import java.awt.Color;
 import java.awt.Point;
+import java.awt.Shape;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
 
 import model.DrawingBoard;
 import view.ViewDrawingBoard;
@@ -12,8 +17,9 @@ public class Controller {
 	private int currentAction = 2;
 	private DrawingBoard drawB = new DrawingBoard();
 	private ViewDrawingBoard viewDrawB = new ViewDrawingBoard(drawB);
-	
-	
+	private Color strokeColor = Color.BLACK;
+	private Color fillColor = Color.BLACK;
+	private float transparentVal = 1.0f;
 	public Controller(){
 		
 	
@@ -27,9 +33,53 @@ public class Controller {
 		    		  System.out.println("PRESSED:"+drawB.getDrawStart());
 		    	  }
 		      }
+		      public void mouseReleased(MouseEvent e){
+		    	  Shape aShape = null; 
+		    	  if(currentAction==2){
+		    		  aShape=drawLine(drawB.getDrawStart().x,
+		    				  drawB.getDrawStart().y,
+		    				  drawB.getDrawEnd().x,
+		    				  drawB.getDrawEnd().y);
+		    	  }
+		      }
+		});
+		drawB.addMyMotionActivity(new MouseMotionAdapter() {
+			public void mouseDragged(MouseEvent e){
+				int x = e.getX();
+				int y = e.getY();
+				
+				Shape aShape=null; 
+				strokeColor=fillColor; 
+				aShape = drawBrush(x,y,5,5);
+				
+				drawB.getShapes().add(aShape);
+				drawB.getShapeFill().add(fillColor);
+				drawB.getShapeStroke().add(strokeColor);
+				drawB.getTransPercent().add(transparentVal);
+				
+				drawB.setDrawStart(null);
+				drawB.setDrawEnd(null);
+				drawB.repaint();
+			}
 		});
 	}
 	
+	 private Line2D.Float drawLine(
+             int x1, int y1, int x2, int y2)
+     {
+
+             return new Line2D.Float(
+                     x1, y1, x2, y2);
+     }
+     private Ellipse2D.Float drawBrush(
+             int x1, int y1, int brushStrokeWidth, int brushStrokeHeight)
+     {
+     	
+     	return new Ellipse2D.Float(
+                 x1, y1, brushStrokeWidth, brushStrokeHeight);
+     	
+     }
+
 	public static void main (String[] args){
 		Controller c = new Controller();
 		c.viewDrawB.setVisible(true);
--- src/model/DrawingBoard.java
+++ src/model/DrawingBoard.java
@@ -1,11 +1,18 @@
 package model;
 
+import java.awt.AlphaComposite;
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.RenderingHints;
 import java.awt.Shape;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseMotionAdapter;
+import java.awt.geom.Line2D;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import javax.swing.BorderFactory;
 import javax.swing.JComponent;
@@ -20,9 +27,11 @@ public class DrawingBoard extends JComponent{
 	private static final long serialVersionUID = 1L;
 	private ArrayList<Shape>shapes=new ArrayList<Shape>();
 	private ArrayList<Color>shapeFill=new ArrayList<Color>();
-	private ArrayList<Float>transPercent=new ArrayList<Float>();
+	private ArrayList<Color>shapeStroke=new ArrayList<Color>();
+	private ArrayList<Float> transPercent = new ArrayList<Float>();
 	private Point drawStart; 
 	private Point drawEnd;
+	private Graphics2D graphicSettings;
 	
 	public DrawingBoard(){
 		this.setBackground(Color.WHITE);
@@ -39,6 +48,58 @@ public class DrawingBoard extends JComponent{
 		
 	}
 	
+	public void paint(Graphics g){
+		//Class used to define the shapes to be drawn
+		graphicSettings=(Graphics2D)g;
+		//Antialiasing cleans up the jagged lines
+		graphicSettings.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+		// Defines the line width of the stroke
+         graphicSettings.setStroke(new BasicStroke(4));
+         
+        //Iterators created to cycle through stroked and fills
+         Iterator<Color>strokeCounter=shapeStroke.iterator();
+         Iterator<Color>fillCounter=shapeFill.iterator();
+         
+        //Iterator for transparency
+         Iterator<Float>transCounter=transPercent.iterator();
+         for (Shape s: shapes) {
+        	 //Sets the shapes transparency value
+        	 graphicSettings.setComposite(AlphaComposite.getInstance(
+                        AlphaComposite.SRC_OVER, transCounter.next()));
+        	// Grabs the next stroke from the color arraylist
+         	graphicSettings.setPaint(strokeCounter.next()); 
+         	graphicSettings.draw(s);
+            // Grabs the next fill from the color arraylist
+        	graphicSettings.setPaint(fillCounter.next()); 	
+        	graphicSettings.fill(s);
+        }
+         
+        //Guide shape used for drawing
+        if (drawStart != null && drawEnd != null){
+        	//makes the guide shape transparent
+        	graphicSettings.setComposite(AlphaComposite.getInstance(
+                    AlphaComposite.SRC_OVER, 0.40f));
+        	
+        	// Make guide shape gray for professional look            
+        	graphicSettings.setPaint(Color.LIGHT_GRAY);
+        	Shape aShape = null;
+        	
+        	aShape = drawLine(drawStart.x, drawStart.y,
+            		drawEnd.x, drawEnd.y);
+        	
+        	graphicSettings.draw(aShape);
+        }
+	}
+	
+	 public Line2D.Float drawLine(
+             int x1, int y1, int x2, int y2)
+     {
+
+             return new Line2D.Float(
+                     x1, y1, x2, y2);
+     }
 	
 	public Point getDrawStart() {
 		return drawStart;
@@ -54,6 +115,42 @@ public class DrawingBoard extends JComponent{
 
 	public void setDrawEnd(Point drawEnd) {
 		this.drawEnd = drawEnd;
+	}
+
+	
+	
+	public ArrayList<Shape> getShapes() {
+		return shapes;
+	}
+
+	public void setShapes(ArrayList<Shape> shapes) {
+		this.shapes = shapes;
+	}
+
+	public ArrayList<Color> getShapeFill() {
+		return shapeFill;
+	}
+
+	public void setShapeFill(ArrayList<Color> shapeFill) {
+		this.shapeFill = shapeFill;
+	}
+
+	public ArrayList<Color> getShapeStroke() {
+		return shapeStroke;
+	}
+
+	public void setShapeStroke(ArrayList<Color> shapeStroke) {
+		this.shapeStroke = shapeStroke;
+	}
+
+	
+	
+	public ArrayList<Float> getTransPercent() {
+		return transPercent;
+	}
+
+	public void setTransPercent(ArrayList<Float> transPercent) {
+		this.transPercent = transPercent;
 	}
 
 	public void addMyMouseActivity(MouseAdapter mouse){
